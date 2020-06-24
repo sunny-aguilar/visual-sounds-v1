e4ddf820aca8f889ba0186ed842c9949cde55061
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-24 00:00:18 -0700

    visual sounds

--- app.js
+++ app.js
@@ -35,14 +35,16 @@
     // console.log('Server running at http://127.0.0.1:8081/');
 
 
+    var fs = require("fs");
+    var data = fs.readFileSync('input.txt');
+
+    console.log(data.toString());
+    console.log("Program Ended");
 
 
 
 
 
-
-
-    
 
 }());
 
